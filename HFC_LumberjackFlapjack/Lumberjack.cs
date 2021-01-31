7f5df1592675549c92a44c5469354b97b1ba75e1
Alex Blackman <alexander.blackman@gmail.com>
2021-01-31 17:26:42 +1100

    Add project files.

---
+++ HFC_LumberjackFlapjack/Lumberjack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HFC_LumberjackFlapjack
+{
+    class Lumberjack
+    {
+        public string Name { get; private set; }
+        private Stack<Flapjack> pouch = new Stack<Flapjack>();
+
+
+        public Lumberjack(string name)
+        {
+            Name = name;        
+        }
+        public  void StashFlapjack(Flapjack tasty)
+        {
+            pouch.Push(tasty);
+        }
+
+        public void EatFlapjack()
+        {
+            Console.WriteLine($"{Name} ate a {pouch.Pop()}");
+        }
+    }
+
+    enum Flapjack
+    {
+        Crispy,
+        Soggy, 
+        Browned,
+        Banana,
+    }
+}
---
+++ HFC_LumberjackFlapjack/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HFC_LumberjackFlapjack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<Lumberjack> logBoyz = new Queue<Lumberjack>();
+            Random random = new Random();
+
+
+            string nameOfLumberjack;
+            while ((nameOfLumberjack = Console.ReadLine()) !="")
+            {
+                Console.WriteLine("Please enter the lumberjack's name");
+                string nameOfLumberJack = Console.ReadLine();
+                Lumberjack newJack = new Lumberjack(nameOfLumberJack);
+                Console.WriteLine("How many flapjacks does he have?");
+                string repliedNumber = Console.ReadLine();
+                if (int.TryParse(repliedNumber, out int noOfFJacks)
+                {
+                    for (int i = 0; i <= noOfFJacks; i++)
+                    {
+
+                    }
+                        
+                }
+
+            }
+            
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
